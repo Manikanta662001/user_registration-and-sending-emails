33d6c4da05b8cc0c294d2e6e57b3bf1f95b194fa
Manikanta662001 <gundlurimanikanta142@gmail.com>
2023-05-03 15:01:53 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from app.forms import *
+from django.http import HttpResponse
 # Create your views here.
+def registration(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            USO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            USO.set_password=pw
+            USO.save()
+
+
+            PFO=pfd.save(commit=False)
+            PFO.username=USO
+            PFO.save()
+
+
+            return HttpResponse('Registration is Successfully completed')
+        else:
+            return HttpResponse('data is not valid')
+    return render(request,'registration.html',d)
--- project22/settings.py
+++ project22/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project22.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL='media/'
+
+MEDIA_DIR=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT=MEDIA_DIR
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
